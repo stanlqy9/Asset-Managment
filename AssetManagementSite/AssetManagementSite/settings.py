bef3635912188a0df47b5fe59d09fa168b691f94
Stanley <sn859627@gmail.com>
2024-07-02 11:13:56 -0700

    chore: Update project settings and URLs to include the "assetmanager" app and remove the "homepage" app

--- AssetManagementSite/AssetManagementSite/settings.py
+++ AssetManagementSite/AssetManagementSite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'assetmanager',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,7 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / "homepage" / "templates"
+             #BASE_DIR / "homepage" / "templates"
             ],
         'APP_DIRS': True,
         'OPTIONS': {
