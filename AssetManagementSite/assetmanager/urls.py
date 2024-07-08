8daabe5c232e57f9220af7db4313902c46804af9
Stanley <sn859627@gmail.com>
2024-07-07 17:39:55 -0700

    chore: Update project settings and URLs to include the "assetmanager" app and remove the "homepage" app

--- AssetManagementSite/assetmanager/urls.py
+++ AssetManagementSite/assetmanager/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("dashboard", views.dashboard),
+    path("all", views.all),
+    path("create", views.create_an_asset),
 ]
--- AssetManagementSite/assetmanager/views.py
+++ AssetManagementSite/assetmanager/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 
 def dashboard(request):
-    return render(request, "assetmanager/main.html")
+    return render(request, "assetmanager/dashboard.html")
+
+def all(request):
+    return render(request, "assetmanager/all.html")
+    
+def create_an_asset(request):
+    return render(request, "assetmanager/create.html")
