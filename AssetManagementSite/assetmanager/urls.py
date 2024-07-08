c479dd448e541da4b534dba7ad843b1698d87d22
Stanley <sn859627@gmail.com>
2024-07-07 19:10:15 -0700

    chore: Update assetmanager templates
    This commit updates the assetmanager templates to include additional HTML elements and classes for styling. The "all.html" template now includes a div with the id "asset-table" to wrap the table. The "create.html" template includes a div with the class "create-form" to wrap the form.

--- AssetManagementSite/assetmanager/urls.py
+++ AssetManagementSite/assetmanager/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("dashboard", views.dashboard),
-    path("all", views.all),
-    path("create", views.create_an_asset),
+    path("dashboard", views.dashboard, name = "dashboard"),
+    path("all", views.all, name = "all"),
+    path("create", views.create_an_asset, name="create"),
+    path("", views.page_redirect),
+    path("<other_path>", views.throw_error)
 ]
--- AssetManagementSite/assetmanager/views.py
+++ AssetManagementSite/assetmanager/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.http import HttpResponseNotFound
 
 
 def dashboard(request):
@@ -9,3 +11,11 @@ def all(request):
     
 def create_an_asset(request):
     return render(request, "assetmanager/create.html")
+
+def page_redirect(request):
+    return redirect("dashboard")
+
+def throw_error(request, other_path):
+    response_data = render(request, "assetmanager/404.html")
+    return HttpResponseNotFound(response_data)
+
